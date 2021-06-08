f912c1abe99fec10ad7e2e577c75fff811c3e2da
marcel0ll <marcelo@lotuz.dev>
2021-06-08 19:46:13 -0300

    Igore perf folder and some other files

--- perf.sh
+++ perf.sh
@@ -1,6 +1,6 @@
-time perf record -F 99 -a -g -- ./jsminify test/comp/phaser.js > phaser.min.js
-perf script > out.perf
-../../FlameGraph/stackcollapse-perf.pl out.perf > out-folded.perf
+time perf record -F 99 -a -g -- ./jsminify test/comp/phaser.js > perf/phaser.min.js
+perf script > perf/out.perf
+../../FlameGraph/stackcollapse-perf.pl perf/out.perf > perf/out-folded.perf
 DAY=$(date +%F)
 DATE=$(date +%s)
 VERSION=$(head package.json -n 3 | tail -n 1 | sed -En "s/.* \"(.*)\".*/\1/p")
@@ -11,5 +11,5 @@ VERSION=$(head package.json -n 3 | tail -n 1 | sed -En "s/.* \"(.*)\".*/\1/p")
   --title "jsminify-$VERSION" \
   --subtitle "$DAY" \
   --cp \
-  out-folded.perf > "perf/perf-graph-$DATE.svg"
+  perf/out-folded.perf > "perf/perf-graph-$DATE.svg"
 
